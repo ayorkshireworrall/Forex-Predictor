c02f5b3f3e4a4ba709dc9616ca0731e2723ee2c2
Alexander Worrall <a.worrall2512@googlemail.com>
2021-02-17 20:31:48 +0000

    Add a couple more methods

--- forex_predictor/process_raw_data.py
+++ forex_predictor/process_raw_data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from datetime import timedelta
 
 #Aim is to conert open-high-low-close (ohlc) data into inputs and output categories for training a model
@@ -8,6 +9,7 @@ from datetime import timedelta
 #variables
 intervals = [] #input ohlc data periods to create, working from right to left decides how far from the target date
 target_interval = timedelta(minutes=60) #The size of the target interval (for buying/selling)
+market = 'EUR/GBP'
 
 #setters
 def set_intervals(interval_array):
@@ -22,6 +24,10 @@ def set_target_interval(interval):
     global target_interval
     target_interval = interval
 
+def set_market(new_market):
+    global market
+    market = new_market
+
 #getters(mainly for tests)
 def get_intervals():
     global intervals
@@ -30,4 +36,24 @@ def get_intervals():
 def get_target_interval():
     global target_interval
     return target_interval
-#--------------------------------------------------------------------------------------
+
+def get_market():
+    global market
+    return market
+
+#----------------------------------process raw data from csv---------------------------------------------
+
+def apply_category_label(open, close):
+    if close - open < 0:
+        return 0
+    else:
+        return 1
+
+def load_market_csv(market):
+    market = market.replace('/', '_')
+    return pd.read_csv(f'data/{market}.csv')
+
+def apply_category_label_for_date(dataframe, datetime):
+    open = df.iloc[0]['open']
+    close = df.iloc[-1]['close']
+    return apply_category_label(open, close)
--- tests/test_process_raw_data.py
+++ tests/test_process_raw_data.py
@@ -1,12 +1,11 @@
-import unittest, sys, os
-path = os.path.abspath(os.path.dirname('forex_predictor/process_raw_data.py'))
-if path not in sys.path:
-    sys.path.insert(0, path)
+import unittest
 from datetime import timedelta
-from process_raw_data import set_intervals, set_const_intervals, set_target_interval, get_intervals, get_target_interval
+from unittest.mock import patch
+from forex_predictor.process_raw_data import get_market, set_intervals, set_const_intervals, set_market, set_target_interval, get_intervals, get_target_interval, apply_category_label, load_market_csv
 
 
 class Test_Process_Raw_Data(unittest.TestCase):
+
     def test_set_intervals(self):
         intervals = [5, 5, 5]
         set_intervals(intervals)
@@ -21,3 +20,20 @@ class Test_Process_Raw_Data(unittest.TestCase):
         expected_intervals = [3, 3, 3, 3, 3]
         set_const_intervals(3, 5)
         self.assertEqual(expected_intervals, get_intervals())
+
+    def test_set_market(self):
+        market = 'GBP/JPY'
+        set_market(market)
+        self.assertEqual(market, get_market())
+
+    def test_categorise_data(self):
+        self.assertEqual(1, apply_category_label(1.2222, 1.2223))
+        self.assertEqual(0, apply_category_label(1.2223, 1.2222))
+
+    @patch('forex_predictor.process_raw_data.pd')
+    def test_load_market_csv(self, mock_pd):
+        load_market_csv('EUR/GBP')
+        mock_pd.read_csv.assert_called_with('data/EUR_GBP.csv')
+
+if __name__ == '__main__':
+    unittest.main()
